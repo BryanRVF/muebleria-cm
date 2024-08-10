33092ae622305b4af90670795b341e4a199e6caa
BryanRVF <bryanviveros.f@gmail.com>
2024-08-10 17:26:23 -0500

    Add files via upload

---
+++ inicio1.php
@@ -0,0 +1,106 @@
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Christian\'s Muebles de Melamina</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
+    <link rel="stylesheet" href="estilos.css">
+   
+</head>
+<body>
+    <?php include ('header.php') ?>
+    
+
+    <div class="container my-5">
+
+    
+       
+ 
+ <div class="row">
+            
+            <div class="col-lg-3 col-md-6 mb-3">
+                <div class="card h-100">
+                   
+            <div class="col-lg-3 col-md-6 mb-3">
+                <div class="card h-100">
+                 
+                   
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="container my-8">
+     
+            
+        <div class="row">
+           
+               
+           
+            </div>
+</div>
+</div>
+<br>
+<br>
+        
+    
+<br>
+<br>
+ <div class="container content text-center">
+        <h1><a class="nav-link"  href="index.php" style="color: black;">INDUSTRIA DEL MUEBLE</h1>
+        <HR>
+        
+        <br>
+        <br>
+        <div class="row ">
+            <div class="col-md-2 ">
+                
+                <img src="imagenes/homieoffice.png" alt="Home Office" class="img-fluid" />
+                <br>
+                <br>
+                
+                <h2>
+    <p><a class="nav-link" href="homeofice.php" style="color: black;">HOME OFFICE</a></p>
+</h2>
+                
+            </div>
+            <div class="col-md-3">
+                <img src="imagenes/oficinaenmelamine.png" alt="Oficinas en Melamina" class="img-fluid" />
+                <br>
+                <br>
+                
+                <h2>
+    <p><a class="nav-link" href="oficinamelamina.php" style="color: black;">OFICINAS DE MELAMINA</a></p>
+</h2>
+            </div>
+            <div class="col-md-3">
+                <img src="imagenes/oficinamoderna.png" alt="Oficinas Modernas" class="img-fluid" />
+                <br>
+                <br>
+                <h2>
+    <p><a class="nav-link" href="baños.php" style="color: black;">MUEBLES DE BAÑO</a></p>
+</h2>
+            </div>
+            <div class="col-md-3">
+                <img src="imagenes/ideas.png" alt="Diseñamos tu Idea" class="img-fluid" />
+                <br>
+                <br>
+
+                <h2> <p><a class="nav-link"  style="color: black; "> DISEÑAMOS TU IDEA </p></a> </h2>
+            </div>
+        </div>
+
+        <?php include('footer.php') ?>
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</body>
+</html>
+
